62398cb11676ef8041e9c339866769ef0f5e9b14
fgalvanbaez <francisco.galvan.baez@gmail.com>
2015-05-28 17:03:40 +0100

    Añado los fichero de nuevo

---
+++ src/Pruebas/pruebaTests.java
@@ -0,0 +1,33 @@
+package Pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ull.etsii.modelado.clasificador.herramientas.Aleatorio;
+
+
+public class pruebaTests {
+
+	public pruebaTests() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		Aleatorio rand = new Aleatorio();
+		rand.generarAleatorios(5);
+		//fail("Not yet implemented");
+	}
+
+}
